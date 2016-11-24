d15e44c82831d279448461e2e38b5e8032da9b05
Zekun Wang <zwang172@gmail.com>
2016-11-24 01:20:04 -0600

    Add category cell, icon images, color and icon enums

---
+++ Extr/CategoryCell.swift
@@ -0,0 +1,63 @@
+//
+//  CategoryCell.swift
+//  Extr
+//
+//  Created by Zekun Wang on 11/24/16.
+//  Copyright © 2016 Expense Manager. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var iconBackgroundImageView: UIImageView!
+    @IBOutlet var iconLabel: UILabel!
+    
+    var category: RCategory! {
+        didSet {
+            iconBackgroundImageView.backgroundColor = UIColor.HexToColor(hexString: category.color)
+            iconImageView.image = UIImage(named: category.icon)
+            iconLabel.text = category.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        iconImageView.image = iconImageView.image?.withRenderingMode(.alwaysTemplate)
+        iconImageView.tintColor = UIColor.white
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension UIColor{
+    static func HexToColor(hexString: String, alpha:CGFloat? = 1.0) -> UIColor {
+        // Convert hex string to an integer
+        let hexint = Int(self.intFromHexString(hexStr: hexString))
+        let red = CGFloat((hexint & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hexint & 0xff00) >> 8) / 255.0
+        let blue = CGFloat((hexint & 0xff) >> 0) / 255.0
+        let alpha = alpha!
+        // Create color object, specifying alpha as well
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return color
+    }
+    
+    static func intFromHexString(hexStr: String) -> UInt32 {
+        var hexInt: UInt32 = 0
+        // Create scanner
+        let scanner: Scanner = Scanner(string: hexStr)
+        // Tell scanner to skip the # character
+        scanner.charactersToBeSkipped = NSCharacterSet(charactersIn: "#") as CharacterSet
+        // Scan hex value
+        scanner.scanHexInt32(&hexInt)
+        return hexInt
+    }
+}
---
+++ Extr/EColor.swift
@@ -0,0 +1,26 @@
+//
+//  EColor.swift
+//  Extr
+//
+//  Created by Zekun Wang on 11/24/16.
+//  Copyright © 2016 Expense Manager. All rights reserved.
+//
+
+import Foundation
+
+enum EColor: String {
+    case blue = "#1DA1F2"
+    case blue_gray = "#848EA5"
+    case brown = "#916A6A"
+    case cyan = "#66BFB3"
+    case deep_purple = "#A17AFF"
+    case green = "#6FBF43"
+    case indigo = "#819DD8"
+    case orange = "#FF8D36"
+    case purple = "#ED72D7"
+    case purple_deep = "#9551A5"
+    case purple_deep_light = "#9E88B2"
+    case red = "#F17071"
+    case yellow = "#F7B314"
+    
+}
