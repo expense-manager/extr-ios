6b897ff784d524916e62bee8b07c7944e60fcf50
Zekun Wang <zwang172@gmail.com>
2016-11-24 01:20:04 -0600

    Add category cell, icon images, color and icon enums

---
+++ Extr/EIcon.swift
@@ -0,0 +1,38 @@
+//
+//  EIcon.swift
+//  Extr
+//
+//  Created by Zekun Wang on 11/24/16.
+//  Copyright © 2016 Expense Manager. All rights reserved.
+//
+
+import Foundation
+
+enum EIcon: String {
+    case drink = "Drink"
+    case bar = "Bar"
+    case restaurant = "Restaurant"
+    case gas_station = "Gas Station"
+    case grocery = "Grocery"
+    case cafe = "Cafe"
+    case taxi = "Taxi"
+    case bus = "Bus"
+    case flight = "Flight"
+    case favorite = "Favorite"
+    case star = "Star"
+    case bubble_chart = "Bubble Chart"
+    case fitness = "Fitness"
+    case music = "Music"
+    case store = "Store"
+    case pets = "Pets"
+    case print_shop = "Print Shop"
+    case business_center = "Business Center"
+    case radio = "Radio"
+    case bowl = "Bowl"
+    case shopping = "Shopping"
+    case apple = "Apple"
+    case cash_used = "Cash Used"
+    case power_plug = "Power Plug"
+    case window_restore = "Window Restore"
+    case security = "Security"
+}
